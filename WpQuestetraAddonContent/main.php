6483a4f8d81e5fe60da58524829e2cb2b0d974b4
yambal <jy@yambal.net>
2017-08-21 18:26:27 +0900

    カードサイズにメリハリを付けた

--- WpQuestetraAddonContent/main.php
+++ WpQuestetraAddonContent/main.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Taxonomy Browser
 Plugin URI: https://github.com/Questetra/WP-Questetra-Addon-Shortcode
 Description: Questetra Addon XML (機能拡張)　ページ関連についてのプラグイン
-Version: 0.5
+Version: 0.6
 Author: June YAMAMOTO
 Author URI: https://www.questetra.com/
 License: GPL2
